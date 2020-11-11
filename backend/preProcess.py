27592269540228b6524da91c93269adb12ca7409
祥 <1073739700@qq.com>
2020-11-11 21:09:50 +0800

    [+] 读取担保关系表并切分子图

--- backend/preProcess.py
+++ backend/preProcess.py
@@ -4,15 +4,15 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def getInitControlG(tPath):
+def getInitControlG(path):
     """
-    读取excel表格到DataFrame
+    读取控制人关系的excel表格到DataFrame
     Param:
-        tPath: 含有控制人数据的excel表格
+        path: 含有控制人数据的excel表格
     Returns: 
         subG: 根据表格数据切分得到的子图集合, 每个元素都是一副子图
     """
-    control = pd.read_csv(tPath, encoding="gb2312")
+    control = pd.read_csv(path, encoding="gb2312")
     # 将列名索引修改为英文
     control.columns = ["relTag", "src", "destn", "relType", "rate"]
     # 将比例大于100的异常值修正为100，并归一化
@@ -24,10 +24,11 @@ def getInitControlG(tPath):
     # print(control)
 
     # Control关系中，relTag和src一一对应
-    # relTag共39910
     G = nx.DiGraph()
-    # 控制人共70420个节点, 构建初始图G
+    # 构建初始图G, 控制人共70420个节点
     for _, row in control.iterrows():
+        G.add_node(row["src"], isRoot="0")
+        G.add_node(row["destn"], isRoot="0")
         G.add_edge(
             row["src"],
             row["destn"],
@@ -40,6 +41,92 @@ def getInitControlG(tPath):
     subG = list()
     for c in nx.connected_components(tmp):
         subG.append(G.subgraph(c))
+    # 各个子图的节点数量
+    # nodesNum = dict()
+    # for item in subG:
+    #     if len(item.nodes()) not in nodesNum:
+    #         nodesNum[len(item.nodes())] = 0
+    #     nodesNum[len(item.nodes())] += 1
+    # print(nodesNum)
+    return subG
+
+
+def getInitGuaranteeG(path):
+    """
+    读取担保关系的excel表格到DataFrame
+    Param:
+        path: 含有担保关系数据的excel表格
+    Returns: 
+        subG: 根据表格数据切分得到的子图集合, 每个元素都是一副子图
+    """
+    guarantee = pd.read_csv(path, encoding="gb2312")
+    guarantee.columns = ["src", "destn", "time", "guarType", "amount"]
+    # 每个样本的担保时间都相同, 没有分析意义, 直接删除
+    guarantee.drop(["time"], axis=1)
+    # 担保金额为0的样本视为无效的担保, 直接删去
+    guarantee = guarantee[~guarantee["amount"].isin([0])]
+    # print(guarantee)
+
+    # 构建初始图G, 一共18711个节点, 14494条边
+    G = nx.DiGraph()
+    for _, row in guarantee.iterrows():
+        G.add_node(row["src"])
+        G.add_node(row["destn"])
+        G.add_edge(
+            row["src"], row["destn"], guarType=row["guarType"], amount=row["amount"],
+        )
+    # 切分子图
+    tmp = nx.to_undirected(G)
+    subG = list()
+    for c in nx.connected_components(tmp):
+        subG.append(G.subgraph(c))
+    # print(len(subG))  # 7158个子图
+    # 各个子图的节点数量
+    nodesNum = dict()
+    for item in subG:
+        if len(item.nodes()) not in nodesNum:
+            nodesNum[len(item.nodes())] = 0
+        nodesNum[len(item.nodes())] += 1
+        if len(item.nodes()) == 23:
+            nx.draw(
+                item,
+                # pos=nx.spring_layout(G),
+                # with_labels=True,
+                # label_size=1000,
+                # node_size=1000,
+                # font_size=20,
+                # edge_labels=edge_labels,
+            )
+            plt.show()
+    print(nodesNum)
+    return subG
+
+
+def getRoot(subG, metric, threshold):
+    """
+    根据指标获取子图的根节点
+    Params:
+        subG: 原子图的列表
+        metric: 判断的指标
+        threshold: 指标的阈值
+    Returns:
+        subG: 更新根节点标记的子图
+    """
+    # 标记各个子图的根节点
+    for G in subG:
+        # 仅含有2个节点的子图
+        if nx.number_of_nodes(G) <= 2:
+            for n in G.nodes:
+                if G.in_degree(n) == 0:
+                    G.nodes[n]["isRoot"] = 1
+                    break  # 仅有一个根, 找到即可退出
+        # 含有3个节点的子图
+        if nx.number_of_nodes(G) <= 2:
+            for u, v in G.edges_iter():
+                if G.in_degree(u) == 0 and G[u][v][metric] >= threshold:
+                    G.nodes[n]["isRoot"] = 1  # 可能存在2个根
+                    # print(n, G.nodes[n]["isRoot"])
+        # 含有3个节点以上的子图
     return subG
 
 
@@ -55,14 +142,10 @@ def getSubgraphFromInitG(G):
 
 
 if __name__ == "__main__":
-    controlG = getInitControlG("./backend/res/control.csv")
-    # 各个子图的节点数量
-    nodesNum = dict()
-    for item in controlG:
-        if len(item.nodes()) not in nodesNum:
-            nodesNum[len(item.nodes())] = 0
-        nodesNum[len(item.nodes())] += 1
-    print(nodesNum)
+    # controlG = getInitControlG("./backend/res/control.csv")
+    # controlG = getRoot(controlG, "rate", 0.5)
+    guaranteeG = getInitGuaranteeG("./backend/res/guarantee.csv")
+
     # nx.draw(
     #     controlG[5],
     #     # pos=nx.spring_layout(G),
