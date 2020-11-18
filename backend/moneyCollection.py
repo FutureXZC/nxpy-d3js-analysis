53c4dc4d3f23803e5e32089df247417341a99e2a
FutureXZC <1073739700@qq.com>
2020-11-18 10:46:13 +0800

    [!] 采用csv包读取资金归集表

--- backend/moneyCollection.py
+++ backend/moneyCollection.py
@@ -1,12 +1,13 @@
 import re
 import json
-import math
+# import math
+import csv
 import datetime
 import numpy as np
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
-import openpyxl
+# import openpyxl
 
 
 def getInitmoneyCollectionG(path):
@@ -17,102 +18,80 @@ def getInitmoneyCollectionG(path):
     Returns: 
         subG: 根据表格数据切分得到的子图集合, 每个元素都是一副子图
     """
-    # 由于原csv中存在非utf8字符，需先将其读取为二进制文件流，过滤掉非uft8字符
-    # with open(path, "rb") as csv_in:
-    #     # 过滤后的内容存在temp.csv中
-    #     with open("./backend/res/temp.csv", "w", encoding="utf-8") as csv_temp:
-    #         for line in csv_in:
-    #             if not line:
-    #                 break
-    #             else:
-    #                 line = line.decode("utf-8", "ignore")
-    #                 csv_temp.write(str(line).rstrip() + "\n")
-    # 通过处理完成后的temp.csv读取资金归集数据
-    moneyCollection = pd.read_csv("./backend/res/temp.csv", encoding="utf8")
-    # 将时间处理为datetime格式，便于后期计算
-    txnDate = [
-        str(x)[0:4] + "-" + str(x)[4:6] + "-" + str(x)[6:8]
-        for x in moneyCollection["jioyrq"]
-    ]
-    txnTime = map(str, moneyCollection["jioysj"])
-    txnTime = [
-        str(x // 10000) + ":" + str(x % 10000 // 100) + ":" + str(x % 100)
-        for x in moneyCollection["jioysj"]
-    ]
-    txnDateTime = [
-        datetime.datetime.strptime(x[0] + " " + x[1], "%Y-%m-%d %H:%M:%S")
-        for x in zip(txnDate, txnTime)
-    ]
-    # 仅保留有价值的列，其余数据删除
-    moneyCollection = moneyCollection[
-        [
-            "zhangh",  # 本人账号
-            "duifzh",  # 对方账户
-            "jiaoym",  # 交易码
-            "jio1je",  # 交易金额
-            # "txnDateTime",  # 交易日期和时间
-            "jiedbz",  # 借贷标志
-            "jiluzt",  # 记录状态
-            "zhyodm",  # 摘要
-        ]
-    ]
-    # txn: transaction, recip: reciprocal
-    moneyCollection.columns = [
-        "myId",  # 本人账号
-        "recipId",  # 对方账户
-        "txnCode",  # 交易码
-        "txnAmount",  # 交易金额
-        # "txnDateTime",  # 交易日期和时间
-        "isLoan",  # 借贷标志
-        "status",  # 记录状态
-        "abstract",  # 摘要
-    ]
-    moneyCollection.insert(4, "txnDateTime", txnDateTime)
-
-    moneyCollection["status"].fillna(-1, inplace=True)  # 将空值替换为-1
-    moneyCollection[["isLoan", "txnAmount"]].astype("float")
-    moneyCollection[["myId", "recipId", "txnCode", "abstract", "status"]].astype("str")
-
+    # 由于原csv中存在非utf-8字符，需先将其读取为二进制文件流，过滤掉非uft-8字符
+    f = open("./backend/res/moneyCollection.csv", encoding='utf-8', errors='ignore')
     # 给定的识别贷款和转账的条件
-    # loan = {
-    #     "code": ['EK95','8002','8003','7743'],
-    #     "isLoan": 0,
-    #     "loanAmountLimit": 100000.0
-    # }
-    # txn = {
-    #     "code": ['6101' ,'6102', '6104' , '61' , '6151' , '2202' , '6002' , '6003' ,
-    #             '6005' , '6006' , '7641' , '7799', '7641' ,'7810', 'DK06' , 'DK05'],
-    #     "txnAmountLimit": 100000.0,
-    #     "isLoan": 1,
-    #     "status": [0, "0"],
-    #     "abstract": ['贷款还款', '委托贷款收回利息', '委托贷款收回本金',
-    #                 '现金管理子账户占用上存金额补足本次扣款', '公积金放款', '贷款并账']
-    # }
-    # moneyCollection = moneyCollection.loc[
-    #     (moneyCollection["txnAmount"] >= loan["loanAmountLimit"])
-    # ]
-    # # 12969条贷款数据
-    # loantable = moneyCollection.loc[(moneyCollection["isLoan"] == loan["isLoan"])]
-    # loantable = loantable[loantable['txnCode'].isin(loan["code"])]
-    # # 4399条转账数据
-    # txntable = moneyCollection.loc[(moneyCollection["isLoan"] == txn["isLoan"])]
-    # txntable = txntable[txntable['status'].isin(txn["status"])]
-    # txntable = txntable[txntable['txnCode'].isin(txn["code"])]
-    # for _, row in txntable.iterrows():
-    #     for s in txn["abstract"]:
-    #         if re.search(s, row["abstract"]):
-    #             txntable = txntable.drop(
-    #                 txntable[(txntable.txnDateTime == row["txnDateTime"]) 
-    #                 & (txntable.myId == row["myId"])].index)
-    #             break
-    # # 共17368条边
-    # moneyCollection = pd.concat([loantable, txntable])
+    # txn: transaction, recip: reciprocal
+    loan = {
+        "code": ['EK95','8002','8003','7743'],
+        "isLoan": "0",
+    }
+    txn = {
+        "code": ['6101' ,'6102', '6104' , '61' , '6151' , '2202' , '6002' , '6003' ,
+                '6005' , '6006' , '7641' , '7799', '7641' ,'7810', 'DK06' , 'DK05'],
+        "txnAmountLimit": 100000.0,
+        "isLoan": "1",
+        "status": "0",
+        "abstract": ['贷款还款', '委托贷款收回利息', '委托贷款收回本金',
+                    '现金管理子账户占用上存金额补足本次扣款', '公积金放款', '贷款并账']
+    }
+    originData = csv.reader(f)
+    tag = {
+        "myId": 0,  # 本人账户
+        "recipId": 29,  # 对方账户
+        "jioyrq": 1,  # 交易日期
+        "jioysj": 3,  # 交易时间
+        "txnCode": 4,  # 交易码
+        "txnAmount": 7,  # 交易金额
+        "isLoan": 6,  # 借贷标志
+        "status": -1,  # 状态码
+        "abstract": 21  # 摘要
+    }
+    moneyCollection = list()
+    for line in originData:
+        ## 忽略标题行
+        if line[0] == 'zhangh':
+            continue
+        ## 贷款和转账条件筛选
+        # 交易金额应大于10万
+        if float(line[tag["txnAmount"]]) < txn["txnAmountLimit"]:
+            continue
+        # 贷款条件筛选
+        if line[tag["isLoan"]] == loan["isLoan"] and line[tag["txnCode"]] in loan["code"]:
+            pass
+        # 转账条件筛选
+        elif line[tag["isLoan"]] == txn["isLoan"] and line[tag["status"]] == txn["status"] and line[tag["txnCode"]] in txn["code"]:
+            flag = False
+            for item in txn["abstract"]:
+                if re.search(item, line[tag["abstract"]]):
+                    flag = True
+                    break
+            if flag:
+                continue
+        else:
+            continue
+        ## 日期格式处理
+        # 将时间处理为datetime格式，便于后期计算
+        txnDate = line[tag["jioyrq"]][0:4] + "-" + line[tag["jioyrq"]][4:6] + "-" + line[tag["jioyrq"]][6:8]
+        x = int(line[tag["jioysj"]])
+        txnTime = str(x // 10000) + ":" + str(x % 10000 // 100) + ":" + str(x % 100)
+        txnDateTime = datetime.datetime.strptime(txnDate + " " + txnTime, "%Y-%m-%d %H:%M:%S")
+        moneyCollection.append({
+            "myId": line[tag["myId"]],
+            "recipId": line[tag["recipId"]],
+            "txnDateTime": txnDateTime,
+            "txnAmount": line[tag["txnAmount"]],
+            "isLoan": line[tag["isLoan"]],
+            "abstract": line[tag["abstract"]]
+        })
 
-    # 构建初始图G
+    ## 构建初始图G, 17368条边
     G = nx.DiGraph()
-    for _, row in moneyCollection.iterrows():
-        G.add_node(row["myId"], matchLoan="", matchTxn="")
-        G.add_node(row["recipId"], matchLoan="", matchTxn="")
+    for row in moneyCollection:
+        if row["myId"] not in G.nodes():
+            G.add_node(row["myId"], matchLoan="", matchTxn="")
+        if row["recipId"] not in G.nodes():
+            G.add_node(row["recipId"], matchLoan="", matchTxn="")
         G.add_edge(
             row["myId"],
             row["recipId"],
@@ -121,8 +100,7 @@ def getInitmoneyCollectionG(path):
             isLoan=row["isLoan"],
             abstract=row["abstract"],
         )
-    print(nx.number_of_nodes(G))
-    # 切分90个子图
+    # 切分67个子图
     tmp = nx.to_undirected(G)
     subG = list()
     for c in nx.connected_components(tmp):
@@ -134,6 +112,7 @@ def getInitmoneyCollectionG(path):
             nodesNum[len(item.nodes())] = 0
         nodesNum[len(item.nodes())] += 1
     print(nodesNum)
+    print(sum(nodesNum.values()))
     return subG
 
 def findShellEnterprise(GList):
@@ -153,8 +132,8 @@ def findShellEnterprise(GList):
             father = list(subG.predecessors(n))
             # print(children, father)
             if len(children) == 0 or len(father) == 0:
-                count += 1
                 continue
+            count += 1
             for f in father:
                 # 寻找最匹配的贷款和转账
                 bestMatchC, bestMatchRate = "", 0.9
